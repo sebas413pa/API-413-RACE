33fd1c78443014c3f0d449e8876bc7a72d83ae56
sebas413pa <sebasic_7@hotmail.com>
2025-11-01 22:17:52 -0600

    fix(customers): Implementación de authenticateJWT
    Implementación de middleware a la ruta que actualiza la información de un cliente

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -6,7 +6,7 @@ const { checkRole } = require('../middlewares/roleMiddleware');
 
 router.get('/', authenticateJWT, checkRole(['Administrador', 'Empleado', 'Cliente']), customerController.listCustomers);
 router.post('/',  customerController.createCustomer);
-router.put('/:customer_id', checkRole(['Administrador', 'Empleado', 'Cliente']), customerController.updateCustomer);
+router.put('/:customer_id', authenticateJWT, checkRole(['Administrador', 'Empleado', 'Cliente']), customerController.updateCustomer);
 router.patch('/activate/:customer_id', authenticateJWT, checkRole(['Administrador', 'Empleado']), customerController.activateCustomer);
 router.patch('/deactivate/:customer_id', authenticateJWT, checkRole(['Administrador', 'Empleado']), customerController.deactivateCustomer);
 router.get('/cities', customerController.listCities);
