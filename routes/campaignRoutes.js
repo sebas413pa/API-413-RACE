dc1d223853964c26407714acc68c7a1d251e0a72
ConstanzaCif <117744459+ConstanzaCif@users.noreply.github.com>
2025-11-14 05:15:49 -0600

    feat(correos seguimiento)

--- routes/campaignRoutes.js
+++ routes/campaignRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { processCampaign } = require('../controllers/campaignController');
+const { processCampaign, sendQuotationsFollowup } = require('../controllers/campaignController');
+const { authenticateJWT } = require('../middlewares/authMiddleware');
+const { checkRole } = require('../middlewares/roleMiddleware');
 
 
 router.post('/webhook', processCampaign);
+router.post('/send-quotations-followup', authenticateJWT, checkRole(['Administrador','Empleado']), sendQuotationsFollowup);
 
 module.exports = router;
